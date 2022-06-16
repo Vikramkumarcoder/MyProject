ee7c9f20506be9c6a430ff9f6a609b8b70dcf283
Vikramkumarcoder <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-06-16 17:22:26 +0530

    Add files via upload

---
+++ age factor.py
@@ -0,0 +1,16 @@
+print('What is your age')
+age = int(input())
+if age>100 or age<7:
+	print('invalid age')
+
+#elif age < 7:
+	#print('invalid age')
+
+elif age>18:
+	print('You can drive👍')
+
+elif age==18:
+	print('We will think 🙏')
+
+else:
+	print('You cannot drive👎')		
