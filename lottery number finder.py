76b5526f403666bd2fd2bdf025042b88c39fd63a
Vikram Kumar <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-06-21 11:56:08 +0530

    Add files via upload

---
+++ lottery number finder.py
@@ -0,0 +1,10 @@
+# Lottery number finder
+
+while True:
+	i=int(input('Enter your 3 digits lottery number\n'))
+	if i>101 and i<201:
+		print('Congratulation you have won $100000000\n')
+		break
+	else:
+		print('Try again!\n')
+		continue
