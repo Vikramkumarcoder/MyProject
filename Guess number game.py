f1fe99d8c44211229bff49772f4ac0e0ee651f4f
Vikram Kumar <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-06-23 18:38:51 +0530

    Update Guess number game.py
    In this game, the user can guess the number and if he/she guesses in limited chances correctly then he/she won the game.

--- Guess number game.py
+++ Guess number game.py
@@ -1,20 +1,15 @@
-while(True):
-	i2 = 9
-	i1= int(input('Guess two digits number\n')) 
-	if i1==18:
-		print('You won the game', i2-1)
-		break
-		i2=i2-1
-	elif i1<18:
-		print('Number is less than \n' , 'Try again!\n', i2-1)
-		continue
-		i2=i2-1
-	elif i1>18:
-		print('Number is more than required value\n' , 'Try again!\n', i2-1)
-		continue
-		i2=i2-1
-	if i2==9:
-		break
-		i2=i2-1
-	else:
-		continue
+chance = 9
+while True:
+    user_input = int(input('Guess two digits number:\n'))
+    if user_input == 18:
+        print("You won the game\nLeft chances:", chance)
+        break
+    if chance == 0:
+        print("Game Over")
+        break
+    elif user_input < 18:
+        print('Number is less than the required value\nTry again!\nLeft chances:', chance)
+        chance = chance - 1
+    elif user_input > 18:
+        print('Number is more than the required value\nTry again!\nLeft chances:', chance)
+        chance = chance - 1
