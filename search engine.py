cd20e4bf4c197b83dea20a916b3ee4e41a1b2c26
Vikram Kumar <104130605+Vikramkumarcoder@users.noreply.github.com>
2022-07-13 19:14:55 +0530

    Add files via upload

---
+++ search engine.py
@@ -0,0 +1,10 @@
+text = input()
+word = input()
+
+def search(text,word):
+        if word in text:
+            return ("Word found")    
+        elif word not in text:
+            return ("Word not found") 
+
+print(search(text, word)))
